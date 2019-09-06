e214d785cdba4563ad878b943370eacc662e80fa
saturnius-cmd <54778601+saturnius-cmd@users.noreply.github.com>
2019-09-06 15:10:28 -0400

    Update varazslat.exe.bat

--- varazslat.exe.bat
+++ varazslat.exe.bat
@@ -1,2 +1,79 @@
 @echo off
-timeout /t 10
+echo Szeretne látni egy varázslatos trükköt?
+echo ...
+echo ...
+echo A folytatáshoz nyomja meg az Enter billentyűt
+pause >nul
+cls
+timeout /t 2 /nobreak >nul
+echo %RANDOM%%RANDOM%kis nyuszi%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%kártyázás%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%eltűnés%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%egy érme a fül mögött%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%varázsige%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%vágja fel a hölgyet felére%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%tej az újságban%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%kis nyuszi%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%kártyázás%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%eltűnés%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%egy érme a fül mögött%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%varázsige%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%vágja fel a hölgyet felére%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%tej az újságban%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%kis nyuszi%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%kártyázás%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%eltűnés%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%egy érme a fül mögött%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%varázsige%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%vágja fel a hölgyet felére%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%tej az újságban%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%kis nyuszi%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%kártyázás%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%eltűnés%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%egy érme a fül mögött%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%varázsige%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%vágja fel a hölgyet felére%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%tej az újságban%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%kis nyuszi%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%kártyázás%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%eltűnés%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%egy érme a fül mögött%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%varázsige%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%vágja fel a hölgyet felére%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%tej az újságban%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%kis nyuszi%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%kártyázás%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%eltűnés%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%egy érme a fül mögött%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%varázsige%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%vágja fel a hölgyet felére%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%tej az újságban%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%kis nyuszi%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%kártyázás%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%eltűnés%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%egy érme a fül mögött%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%varázsige%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%vágja fel a hölgyet felére%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%tej az újságban%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%kis nyuszi%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%kártyázás%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%eltűnés%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%egy érme a fül mögött%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%varázsige%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%vágja fel a hölgyet felére%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%tej az újságban%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%kis nyuszi%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%kártyázás%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%eltűnés%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%egy érme a fül mögött%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%varázsige%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%vágja fel a hölgyet felére%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%tej az újságban%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%kis nyuszi%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%kártyázás%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%eltűnés%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%egy érme a fül mögött%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%varázsige%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%vágja fel a hölgyet felére%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+echo %RANDOM%%RANDOM%tej az újságban%RANDOM%%RANDOM%%RANDOM%> %RANDOM%.txt
+timeout /t 2 /nobreak >nul
